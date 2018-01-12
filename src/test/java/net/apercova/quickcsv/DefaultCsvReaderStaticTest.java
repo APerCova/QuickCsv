783c8939725defb1b3fc472eb20bdb62e5f24e42
A. PerCova <alonsopc91@gmail.com>
2018-01-12 16:32:00 -0600

    Update and rename ObjectCsvReaderStaticTest.java to DefaultCsvReaderStaticTest.java

--- src/test/java/net/apercova/quickcsv/DefaultCsvReaderStaticTest.java
+++ src/test/java/net/apercova/quickcsv/DefaultCsvReaderStaticTest.java
@@ -9,7 +9,7 @@ import java.util.List;
 import org.junit.Assert;
 import org.junit.Test;
 
-public class ObjectCsvReaderStaticTest {
+public class DefaultCsvReaderStaticTest {
 
     public InputStream inStream;
     public Reader reader;
@@ -17,16 +17,50 @@ public class ObjectCsvReaderStaticTest {
     @Test
     public void test01() throws Exception {
 
-        System.out.println("========== ObjectCsvReaderStaticTest#test01()==========");
+        System.out.println("========== SimpleCsvReaderStaticTest#test01()==========");
         try{
         	inStream = ClassLoader.getSystemResourceAsStream("Months.csv");
             reader = new InputStreamReader(inStream);
-            //Default RFC 4180 format
-            List<Month> values = ObjectCsvReader.read(Month.class, reader);
+            //Defaulr RFC 4180 format
+            List<List<String>> values = DefaultCsvReader.read(reader);
 
             Assert.assertTrue(values != null);
-            for(Month row: values){
-                Assert.assertTrue("Row: " + String.valueOf(row),row.getM12().length() > 0);
+            for(List<String> row: values){
+                Assert.assertTrue("Row: " + String.valueOf(row),row.size() == 12);
+                System.out.println("ROW: "+row+"");
+            }
+
+            Assert.assertTrue("Size: "+values.size(), values.size() == 6);
+        }catch(CsvReaderException e){
+            Assert.fail(e.getMessage());
+            e.printStackTrace();
+        }finally {
+            try{
+                inStream.close();
+                reader.close();
+            }catch (IOException e){
+                e.printStackTrace();
+            }
+        }
+
+        System.out.println("======================================================");
+
+    }
+    
+    @Test
+    public void test02() throws Exception {
+
+        System.out.println("========== SimpleCsvReaderStaticTest#test02()==========");
+        try{
+        	inStream = ClassLoader.getSystemResourceAsStream("Months.csv");
+            reader = new InputStreamReader(inStream);
+            //Defaulr RFC 4180 format escaping header
+            List<List<String>> values = DefaultCsvReader.read(
+            		reader, CsvCons.COMMA, CsvCons.DOUBLE_QUOTE, true, 0, 0);
+
+            Assert.assertTrue(values != null);
+            for(List<String> row: values){
+                Assert.assertTrue("Row: " + String.valueOf(row),row.size() == 12);
                 System.out.println("ROW: "+row+"");
             }
 
@@ -48,19 +82,19 @@ public class ObjectCsvReaderStaticTest {
     }
     
     @Test  
-    public void test02() throws Exception {
+    public void test03() throws Exception {
 
-        System.out.println("========== ObjectCsvReaderStaticTest#test02()==========");
+        System.out.println("========== SimpleCsvReaderStaticTest#test03()==========");
         try{
         	inStream = ClassLoader.getSystemResourceAsStream("Months.csv");
             reader = new InputStreamReader(inStream);
             //Default RFC 4180 format limited to a fixed fow range
-            List<Month> values = ObjectCsvReader.read(
-            		Month.class, reader, CsvCons.COMMA, CsvCons.DOUBLE_QUOTE, 1, 3 );
+            List<List<String>> values = DefaultCsvReader.read(
+            		reader, CsvCons.COMMA, CsvCons.DOUBLE_QUOTE, true, 1, 3 );
 
             Assert.assertTrue(values != null);
-            for(Month row: values){
-                Assert.assertTrue("Row: " + String.valueOf(row),row.getM12().length() > 0);
+            for(List<String> row: values){
+                Assert.assertTrue("Row: " + String.valueOf(row),row.size() == 12);
                 System.out.println("ROW: "+row+"");
             }
 
@@ -82,23 +116,23 @@ public class ObjectCsvReaderStaticTest {
     }
     
     @Test
-    public void test03() throws Exception {
+    public void test04() throws Exception {
 
-        System.out.println("========== ObjectCsvReaderStaticTest#test03()==========");
+        System.out.println("========== SimpleCsvReaderStaticTest#test04()==========");
         try{
         	inStream = ClassLoader.getSystemResourceAsStream("MonthsCustom.csv");
             reader = new InputStreamReader(inStream);
             //Custom Format
-            List<Month> values = ObjectCsvReader.read(
-            		Month.class, reader, CsvCons.PIPE, CsvCons.SINGLE_QUOTE, 0, 0);
+            List<List<String>> values = DefaultCsvReader.read(
+            		reader, CsvCons.PIPE, CsvCons.SINGLE_QUOTE, false, 0, 0);
 
             Assert.assertTrue(values != null);
-            for(Month row: values){
-                Assert.assertTrue("Row: " + String.valueOf(row),row.getM12().length() > 0);
+            for(List<String> row: values){
+                Assert.assertTrue("Row: " + String.valueOf(row),row.size() == 12);
                 System.out.println("ROW: "+row+"");
             }
 
-            Assert.assertTrue("Size: "+values.size(), values.size() == 5);
+            Assert.assertTrue("Size: "+values.size(), values.size() == 6);
         }catch(CsvReaderException e){
             Assert.fail(e.getMessage());
             e.printStackTrace();
