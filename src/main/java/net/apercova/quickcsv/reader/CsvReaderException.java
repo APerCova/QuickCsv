98dfd715698f6b398a5bfd268e0ed9e7520c1e94
A. PerCova <alonsopc91@gmail.com>
2018-06-15 16:33:37 -0500

    Create CsvReaderException.java

---
+++ src/main/java/net/apercova/quickcsv/reader/CsvReaderException.java
@@ -0,0 +1,26 @@
+package net.apercova.quickcsv.reader;
+
+/**
+ * Exception for a Csv reading error.
+ * @author <a href="https://twitter.com/apercova" target="_blank">{@literal @}apercova</a> <a href="https://github.com/apercova" target="_blank">https://github.com/apercova</a>
+ * @since 1.0
+ */
+public class CsvReaderException extends Exception{
+
+	private static final long serialVersionUID = 2070949263453202075L;
+
+	public CsvReaderException() {
+    }
+
+    public CsvReaderException(String message) {
+        super(message);
+    }
+
+    public CsvReaderException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public CsvReaderException(Throwable cause) {
+        super(cause);
+    }
+}
